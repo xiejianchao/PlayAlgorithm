0f36f8ddc47702eff5180f5c1145c5c3f57e65f2
elon <elon@elon-ubuntu18.04>
2018-12-10 23:54:32 +0800

    add merge sorted

--- src/com/github/algorithm/MergeSorted.java
+++ src/com/github/algorithm/MergeSorted.java
@@ -1,12 +1,40 @@
 package com.github.algorithm;
 
+import com.github.algorithm.data.BaseData;
+import com.github.algorithm.util.Util;
+
 /**
  * 将两个有序的数组合并为一个有序数组
  */
 public class MergeSorted {
 
     public static void main(String[] args) {
+        int[] mergeSorted = mergeSorted(BaseData.ARR2, BaseData.ARR3);
+        Util.printArr(mergeSorted);
+    }
 
+    private static int[] mergeSorted(int[] arr, int[] arr2) {
+        int[] result = new int[arr.length + arr2.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < arr.length && j < arr2.length) {
+            if (arr[i] < arr2[j]) {
+                result[k++] = arr[i++];
+            } else {
+                result[k++] = arr2[j++];
+            }
+
+        }
+
+        while (i < arr.length) {
+            result[k++] = arr[i++];
+        }
+
+        while (j < arr2.length) {
+            result[k++] = arr2[j++];
+        }
+        return result;
     }
 
 }
--- src/com/github/algorithm/data/BaseData.java
+++ src/com/github/algorithm/data/BaseData.java
@@ -4,4 +4,7 @@ public class BaseData {
 
     public static final int[] ARR = {11, 29, 25, 9, 21, 18, 7, 16};
 
+    public static final int[] ARR2 = {3, 6, 15, 18, 19, 22, 25, 28};
+    public static final int[] ARR3 = {30, 32, 33, 36, 38, 51, 53};
+
 }
