e6e2f5715fdf0fdfc3e67351f1d53fbdc4efbdd1
xiejc <xiejc01@gmail.com>
2019-01-15 23:29:32 +0800

    update

--- src/com/github/algorithm/linked/SingleLinkedListTest.java
+++ src/com/github/algorithm/linked/SingleLinkedListTest.java
@@ -6,11 +6,11 @@ public class SingleLinkedListTest {
 
     public static void main(String[] args) {
         SingleLinkedList link = new SingleLinkedList();
-        link.addHead(1);
-        link.addHead(new Node(2));
-        link.addHead(3);
-        link.addHead(4);
         link.addHead(5);
+        link.addHead(4);
+        link.addHead(3);
+        link.addHead(new Node(2));
+        link.addHead(1);
 
         Log.println("初始链表为：");
         link.print();
@@ -23,8 +23,8 @@ public class SingleLinkedListTest {
         link.reverseLinked();
         Log.println("链表翻转后为：");
         link.print();
-        link.reverseLinked();
-        Log.println("第二次链表翻转后为：");
-        link.print();
+//        link.reverseLinked();
+//        Log.println("第二次链表翻转后为：");
+//        link.print();
     }
 }
